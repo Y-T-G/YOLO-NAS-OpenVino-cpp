7690f866a4fff2650e6dc7caf19f06816fa4662c
Y-T-G <yaseensinbox@gmail.com>
2023-07-07 15:51:11 +0800

    remove timing function

--- include/yolo-nas.hpp
+++ include/yolo-nas.hpp
@@ -24,14 +24,5 @@ public:
     std::vector<int> imgSize;
     YoloNAS(std::string model_path, std::vector<int> imgsz, bool cuda, float scoreTresh, float iouTresh);
     void preprocess(cv::Mat &source, cv::Mat &dst, std::vector<float> &ratios);
-    void predict(cv::Mat &img); 
-    template <
-        class result_t = std::chrono::milliseconds,
-        class clock_t = std::chrono::steady_clock,
-        class duration_t = std::chrono::milliseconds
-    >
-        auto since(std::chrono::time_point<clock_t, duration_t> const& start)
-    {
-        return std::chrono::duration_cast<result_t>(clock_t::now() - start);
-    }
+    void predict(cv::Mat &img);
 };
